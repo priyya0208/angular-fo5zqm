470f5d4ab39ff3ea963784040dba04b81d155ba5
priyya0208 <priyya.0208@gmail.com>
2019-09-16 19:35:42 +0530

    2 files modified, 2 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,18 +9,20 @@ import { RegisterComponent } from './register/register.component';
 import { PagenotfountComponent } from './pagenotfount/pagenotfount.component';
 import { AuthService } from './auth.service';
 import { HomeComponent } from './home/home.component';
+import { LogoutComponent } from './logout/logout.component';
 const appRoutes: Routes = [
   {path:'login', component: LoginComponent},
   {path:'register', component: RegisterComponent},
   {path:'home', component:HomeComponent},
   {path:'', component:LoginComponent},
+  {path:'logout', component:LogoutComponent},
   {path:'**', component:PagenotfountComponent}
 ]
 @NgModule({
   imports:      [ 
     RouterModule.forRoot(appRoutes, {enableTracing:true}),
     BrowserModule, FormsModule ],
-  declarations: [ AppComponent, LoginComponent, RegisterComponent, PagenotfountComponent, HomeComponent ],
+  declarations: [ AppComponent, LoginComponent, RegisterComponent, PagenotfountComponent, HomeComponent, LogoutComponent ],
   bootstrap:    [ AppComponent ],
   providers: [AuthService]
 })
