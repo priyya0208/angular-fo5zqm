941972787f9a2cb6bfdca3f5c6f957dd12149116
priyya0208 <priyya.0208@gmail.com>
2019-09-16 15:30:42 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-fo5zqm
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-fo5zqm)
---
+++ src/app/hello/hello.component.ts
@@ -0,0 +1,9 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'hello',
+  templateUrl: './hello.component.html',
+})
+export class HelloComponent  {
+  title = "Hello";
+}
