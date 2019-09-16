e43b0b68404109f39a7209e72450f753beb1f4a7
priyya0208 <priyya.0208@gmail.com>
2019-09-16 16:02:51 +0530

    3 files modified, 8 added, 3 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello/hello.component';
-
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { PagenotfountComponent } from './pagenotfount/pagenotfount.component';
+const appRoutes: Routes = [
+  {path:'login', component: LoginComponent},
+  {path:'register', component: RegisterComponent},
+  {path:'', component:LoginComponent},
+  {path:'**', component:PagenotfountComponent}
+]
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  imports:      [ 
+    RouterModule.forRoot(appRoutes, {enableTracing:true}),
+    BrowserModule, FormsModule ],
+  declarations: [ AppComponent, LoginComponent, RegisterComponent, PagenotfountComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
