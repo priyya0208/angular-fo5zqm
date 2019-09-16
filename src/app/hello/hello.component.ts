3bff082e6c9a33dd8352757df58008cb3ef4a66d
priyya0208 <priyya.0208@gmail.com>
2019-09-16 15:35:26 +0530

    2 files modified, 1 added

--- src/app/hello/hello.component.ts
+++ src/app/hello/hello.component.ts
@@ -3,7 +3,7 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'hello',
   templateUrl: 'hello.component.html',
-  styles: [`h1 { font-family: Lato; }`]
+  styleUrls : ['hello.component.css']
 })
 export class HelloComponent  {
   @Input() name: string;
